8ab35becf1001242372420b4562607e87a87a394
gerosantacruz <geronimo.santacruz2017@gmail.com>
2019-11-28 23:27:41 -0300

    THe ball and the paddle are having the correct collision

--- pong.py
+++ pong.py
@@ -1,15 +1,15 @@
-#Pong game by Gero
+# Pong game by Gero
 
 import turtle
 
 wm = turtle.Screen()
 wm.title("Pong by Gero")
 wm.bgcolor("black")
-wm.setup(width = 800, height =600)
+wm.setup(width=800, height=600)
 wm.tracer(0)
 
 
-#Paddle A
+# Paddle A
 paddle_a = turtle.Turtle()
 paddle_a.speed(0)
 paddle_a.shape("square")
@@ -18,7 +18,7 @@ paddle_a.shapesize(stretch_wid=5, stretch_len=1)
 paddle_a.penup()
 paddle_a.goto(-350, 0)
 
-#Paddle B
+# Paddle B
 paddle_b = turtle.Turtle()
 paddle_b.speed(0)
 paddle_b.shape("square")
@@ -27,14 +27,78 @@ paddle_b.shapesize(stretch_wid=5, stretch_len=1)
 paddle_b.penup()
 paddle_b.goto(350, 0)
 
-#ball
+# ball
 ball = turtle.Turtle()
 ball.speed(0)
 ball.shape("circle")
 ball.color("white")
 ball.penup()
 ball.goto(0, 0)
+ball.dx = 0.3
+ball.dy = -0.3
 
-#main game loop
+
+# Function
+def paddle_a_up():
+    y = paddle_a.ycor()
+    y += 10
+    paddle_a.sety(y)
+
+
+def paddle_a_down():
+    y = paddle_a.ycor()
+    y -= 10
+    paddle_a.sety(y)
+
+
+def paddle_b_up():
+    y = paddle_b.ycor()
+    y += 10
+    paddle_b.sety(y)
+
+
+def paddle_b_down():
+    y = paddle_b.ycor()
+    y -= 10
+    paddle_b.sety(y)
+
+
+# Listener keyboard binding
+wm.listen()
+wm.onkeypress(paddle_a_up, "w")
+wm.onkeypress(paddle_a_down, "s")
+wm.onkeypress(paddle_b_up, "Up")
+wm.onkeypress(paddle_b_down, "Down")
+
+# main game loop
 while True:
     wm.update()
+
+    # Move the ball across the screen
+    ball.setx(ball.xcor() + ball.dx)
+    ball.sety(ball.ycor() + ball.dy)
+
+    # Border checking for the ball
+    if ball.ycor() > 290:
+        ball.sety(290)
+        ball.dy *= -1
+
+    if ball.ycor() < -290:
+        ball.sety(-290)
+        ball.dy *= -1
+
+    if ball.xcor() > 390:
+        ball.goto(0, 0)
+        ball.dx *= -1
+
+    if ball.xcor() < -390:
+        ball.goto(0, 0)
+        ball.dx *= -1
+
+    if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle_b.ycor() + 50 and ball.ycor() > paddle_b.ycor() - 50):
+        ball.setx(340)
+        ball.dx *= -1
+        
+    if (ball.xcor() < -340 and ball.xcor() > -350) and (ball.ycor() < paddle_a.ycor() + 50 and ball.ycor() > paddle_a.ycor() - 50):
+        ball.setx(-340)
+        ball.dx *= -1
